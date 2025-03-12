a1a439400e1a8d2d50703b74c0e3342a74be3ad5
22250ae <22250ae@hvhs.school.nz>
2025-03-12 20:43:41 +0000

    st

--- script.js
+++ script.js
@@ -9,17 +9,14 @@ function setup() {
 
 	world.gravity.y = 10;
 
-	player = new Sprite(250, 250, 315, 'd');
+	player = new Sprite(250, 250, 100, 100, 'd');
 	player.color = 'YellowGreen';
 	player.vel.x = 2;
 	player.vel.y = 2;
 
-	asteroid = new Sprite(500, 500, 100, 100, 'd');
+	asteroid = new Sprite(100, 100, 100, 'd');
 	asteroid.color = 'black';
-	asteroid.rotationSpeed = 4;
-	asteroid.vel.x = 2;
-	asteroid.vel.y = 2;
-
+	
 	floor = new Sprite(720, GameHeight, GameWidth, 50, 'k');
 
 }
@@ -28,6 +25,36 @@ function setup() {
 function draw() {
 
 	background('SaddleBrown'); 
+
 	
+if (kb.pressing('left')) {
+   player.vel.x = -2;
 }
 
+if (kb.released('left')) {
+   player.vel.x = 0;
+}
+
+if (kb.pressing('right')) {
+   player.vel.x = 2;
+}
+if (kb.released('right')) {
+   player.vel.x = 0;
+}
+
+if (kb.pressing('up')) {
+   player.vel.y = -2;
+}
+if (kb.released('up')) {
+   player.vel.y = 0;
+}
+
+if (kb.pressing('down')) {
+   player.vel.y = 2;
+}
+if (kb.released('down')) {
+   player.vel.y = 0;
+}
+	
+
+}
