62bbcdc39964feb422259693f6b8944f76260cbe
22250ae <22250ae@hvhs.school.nz>
2025-03-13 21:58:21 +0000

    added asteroids made them fall from sky

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 const GameWidth = 1440;
 const GameHeight = 803;
-
-
 function setup() {
 	console.log("setup: ");
 
@@ -9,52 +7,48 @@ function setup() {
 
 	world.gravity.y = 10;
 
-	player = new Sprite(250, 250, 100, 100, 'd');
+	player = new Sprite(250, 750, 100, 100, 'd');
 	player.color = 'YellowGreen';
 	player.vel.x = 2;
 	player.vel.y = 2;
-
-	asteroid = new Sprite(100, 100, 100, 'd');
-	asteroid.color = 'black';
 	
 	floor = new Sprite(720, GameHeight, GameWidth, 50, 'k');
 
-}
-	
+	for (i = 0; i < 10; i++) {
+		asteroid = new Sprite(random(0, 1440), 0);
+		asteroid.vel.x = 0;
+		asteroid.vel.y = 0;
+		asteroid.bounciness = 0.5;
+		asteroid.friction = 100;
+	  }
 
+}
 function draw() {
 
-	background('SaddleBrown'); 
+	background('black'); 
 
-	
 if (kb.pressing('left')) {
-   player.vel.x = -2;
+   player.vel.x = -5;
 }
-
 if (kb.released('left')) {
    player.vel.x = 0;
 }
-
 if (kb.pressing('right')) {
-   player.vel.x = 2;
+   player.vel.x = 5;
 }
 if (kb.released('right')) {
    player.vel.x = 0;
 }
-
 if (kb.pressing('up')) {
-   player.vel.y = -2;
+   player.vel.y = -5;
 }
 if (kb.released('up')) {
    player.vel.y = 0;
 }
-
 if (kb.pressing('down')) {
-   player.vel.y = 2;
+   player.vel.y = 5;
 }
 if (kb.released('down')) {
    player.vel.y = 0;
 }
-	
-
 }
