13c821c48f05a2496fa9209bbfaa73238078b23e
22250ae <22250ae@hvhs.school.nz>
2025-03-18 00:32:03 +0000

    broken timer added

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const GameWidth = 1440;
 const GameHeight = 803;
+let asteroidspawn
+let triggered = false;
 function setup() {
 	console.log("setup: ");
 
@@ -7,57 +9,73 @@ function setup() {
 
 	world.gravity.y = 10;
 
+	asteroidspawn = millis();
+    
 	player = new Sprite(250, 750, 100, 100, 'd');
 	player.color = 'YellowGreen';
 	player.vel.x = 2;
 	player.vel.y = 2;
 	
-	floor = new Sprite(720, GameHeight, GameWidth, 50, 'k');
+	floor = new Sprite(720, GameHeight, 1600, 50, 'k');
+    wall1 = new Sprite(1500, 401, 50, 1500, 'k');
+	wall2 = new Sprite(-60, 401, 50, 1500, 'k');
 
 	asteroidGroup = new Group();
-	for (i = 0; i < 10; i++) {
-		MakeAsteroid();
-	  }
-	 
-
+     
+}
+function spawnasteroid() {
+ MakeAsteroid();
 }
 function MakeAsteroid() {
 	asteroid = new Sprite(random(0, 1440), 0);
-	asteroid.vel.x = 0;
-	asteroid.vel.y = 0;
-	asteroid.bounciness = 0.5;
-	asteroid.friction = 100;
+	asteroid.vel.x = (random(-10, 10));
+	asteroid.vel.y = (random(0, 10));
 	asteroidGroup.add(asteroid);
 }
+
 function draw() {
+	let elapsedTime = millis() - asteroidspawn;
+
+	if (elapsedTime = 10000 && !triggered) {
+		triggered = true;
+		MakeAsteroid();
+		elapsedTime = asteroidspawn
+		triggered = false;
+	  }
+ 
+	
 	function func2Call(asteroid, floor) {
 		asteroid.remove();
 		}
-		
-	background('black'); 
 
+	
+	background(220); 
+//
 	asteroidGroup.collides(floor, func2Call);
+	asteroidGroup.collides(wall1, func2Call);
+	asteroidGroup.collides(wall2, func2Call);
 
+//Keyboard controls
 if (kb.pressing('left')) {
-   player.vel.x = -5;
+   player.vel.x = -8;
 }
 if (kb.released('left')) {
    player.vel.x = 0;
 }
 if (kb.pressing('right')) {
-   player.vel.x = 5;
+   player.vel.x = 8;
 }
 if (kb.released('right')) {
    player.vel.x = 0;
 }
 if (kb.pressing('up')) {
-   player.vel.y = -5;
+   player.vel.y = -8;
 }
 if (kb.released('up')) {
    player.vel.y = 0;
 }
 if (kb.pressing('down')) {
-   player.vel.y = 5;
+   player.vel.y = 8;
 }
 if (kb.released('down')) {
    player.vel.y = 0;
