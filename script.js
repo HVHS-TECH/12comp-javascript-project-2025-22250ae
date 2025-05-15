f37ff696d17cddc6c7aa8214b5df6ee99b395950
22250ae <22250ae@hvhs.school.nz>
2025-05-15 22:37:18 +0000

    test

--- script.js
+++ script.js
@@ -1,123 +1,132 @@
-//setting games width and height
-const GAMEWIDTH = 1440;
-const GAMEHEIGHT = 803;
+//sets all constant variables, these variables will remain the same no matter what
+// TEST - Force a push
+const GAMEWIDTH = 1000;
+const GAMEHEIGHT = 600;
 const PLAYERWIDTH = 50;
 const PLAYERHEIGHT = 100;
 const BUTTONWIDTH = 400;
 const BUTTONHEIGHT = 150;
-//setting up all the variables for Score, Highscore, Time, and when the game stops
+//sets all non constant variables, these variables will change throughout the code
 let time = 0
 let Score = 0
 let Highscore = 0
 let stopgame = true
 let scoregroup;
 function setup() {
-	mouse.visible = false;
 	console.log("setup: ");
+	//Sets canvas
 	cnv = new Canvas(GAMEWIDTH, GAMEHEIGHT);
-
-		world.gravity.y = 10;
-
-	asteroidspawn = millis();
-    
+	//Sets world gravity
+	world.gravity.y = 10;
+	//These are sprites that will be removed when the game starts, so they are seperated from the rest.
 	reset = new Sprite (GAMEWIDTH/2, GAMEHEIGHT/3, BUTTONWIDTH, BUTTONHEIGHT, 'k')
-
-	player = new Sprite(250, 750, PLAYERWIDTH, PLAYERHEIGHT, 'd');
-	player.color = 'Black';
-
-	floor = new Sprite(720, GAMEHEIGHT, 1600, 50, 'k');
-    wall1 = new Sprite(1500, 401, 50, 1500, 'k');
-	wall2 = new Sprite(-60, 401, 50, 1500, 'k');
-	//creates the groups.
+	Instructions = new Sprite (GAMEWIDTH/2., GAMEHEIGHT/1.5, BUTTONWIDTH*2, BUTTONHEIGHT/2, 'k')
+	//These sprites are the sprites that will never be removed
+	player = new Sprite(GAMEWIDTH/2, GAMEHEIGHT, PLAYERWIDTH, PLAYERHEIGHT, 'd');
+	floor = new Sprite(GAMEWIDTH/2, GAMEHEIGHT, 1600, 50, 'k');
+    wall1 = new Sprite(GAMEWIDTH+25, GAMEHEIGHT/2, 50, 1500, 'k');
+	wall2 = new Sprite(-25, GAMEHEIGHT/2, 50, 1500, 'k');
+	//sets the groups
 	asteroidGroup = new Group();
 	Scoregroup = new Group();
 }
-
-
-//function to make asteroids
+//function for making asteroids, using a parameter for the asteroids radius.
 function MakeAsteroid(_asteroidradius) {
-	//spawns sprites at a random spot above the visible area of the game
-	asteroid = new Sprite(random(0, 1440), -50, (_asteroidradius), 'd');
-	//makes asteroids have random velocity to increase the natural feeling of the game
+	//Makes the asteroid sprite
+	asteroid = new Sprite(random(0, 1000), -50, (_asteroidradius), 'd');
+	//Sets the asteroids colour
+	asteroid.color = 'Gray';
+	//Sets they asteroids velocity both X and Y
 	asteroid.vel.x = (random(-10, 10));
 	asteroid.vel.y = (random(0, 10));
-	//adds the new asteroid sprites to the asteroid group so that I can remove or modify all the asteroids at once.
+	//Adds astroids to the asteroid group so I can modify all of them at once
 	asteroidGroup.add(asteroid);
 }
+//Function for making balls after game ends, putting your score into better perspective.
 function MakeBalls() {
-	//spawns balls
-	ball = new Sprite(GAMEWIDTH/2, GAMEHEIGHT/3, 50, 'd');
-	ball.vel.x = (random(-10, 10));
-	ball.vel.y = (random(0, 10));
+	//Makes the ball sprite
+	ball = new Sprite(random(0, 1000), -50, 50, 'd');
+	//Sets the ball colour
+	ball.color = 'Gray';
+	//Adds ball to group so they can all be modified at once
 	Scoregroup.add(ball);
 }
-
-
-//function for when asteroids hit floor
+//function for when the asteroids hit the floor
 function AsteroidHitFloor(asteroid, floor) {
-	//removes asteroid
+	//removes the asteroid that hits the floor
 	asteroid.remove();
-	//adds to the score
+	//adds 1 to the score, the scoring system is based off how many asteroids hit the ground
 	Score++;
 }
-
-
-//function for when asteroid hits player
-function AsteroidHitPlayer() {
-	//stops the game
-		reset = new Sprite (GAMEWIDTH/2, GAMEHEIGHT/3, BUTTONWIDTH, BUTTONHEIGHT, 'k')
-		text('Restart', GAMEWIDTH/2, GAMEHEIGHT/3);
+//Function for when the game ends
+function Gameover() {
+	//Creates the same reset sprite and Instructions sprite from the setup function.
+	reset = new Sprite (GAMEWIDTH/2, GAMEHEIGHT/3, BUTTONWIDTH, BUTTONHEIGHT, 'k')
+	Instructions = new Sprite (GAMEWIDTH/2., GAMEHEIGHT/1.5, BUTTONWIDTH*2, BUTTONHEIGHT/2, 'k')
+	//Makes stopgame true, stopping timer for asteroid spawn, making the mouse visible, and making the player unable to move
 	stopgame = true;
-	//makes mouse visible so you can press reset button
+	//Deletes all remaining asteroids
 	asteroidGroup.removeAll();
-	//sets the players velocity to 0 on both axis
+	//Removes any remaining velocity from the player
 	player.vel.x = (0);
 	player.vel.y = (0);
+	//Makes a ball for every score point
 	for (i = 0; i < Score; i++) {
     	MakeBalls();
 	}
 }
-
-
 function draw() {
-	if (stopgame == true) {
-		mouse.visible = true;
-	}
-	//code for when the reset button is pressed, it starts the game again and deletes the reset button
-	if (mouse.pressing() && reset.mouse.hovering()) {
-		stopgame = false;
-		reset.remove();
-		Scoregroup.removeAll();
-		time = 0;
-		Score = 0;
-	}
-	//sets background colour
-	background(220);
-	//adds to the time variable based off of how much time has passed 
-	time++;
-	//sets the textsize and makes text for the score and highscore
+	//sets the color for all sprites
+	player.color = 'White';
+	reset.color = 'White';
+	floor.color = 'Gray';
+	Instructions.color = 'White';
+	background('Black'); 
+	//all of the text code, starting with text size then the text on sprites then the score and highscore text which is independent from any sprite
+	reset.textSize = 40;
+	Instructions.textSize = 20;
+	reset.text = "Start";	
+	Instructions.text = "A to move left, D to move right, dodge the asteroids!";
 	textSize(32);
-	text('Score: '+Score, 100, 100);
-	text('Highscore: '+Highscore, 100, 200);
-	text('Restart', GAMEWIDTH/2, GAMEHEIGHT/3);
-	//spawns asteroids and resets the time variable every time the time variable reaches 10
-	if (time == 10 && stopgame == false) {
-		MakeAsteroid((random(30, 100)));
-		time = 0;			
-	}
-
-	//sets the score as the highscore if the score is higher than the highscore
-	if (Score >= Highscore) {
-		Highscore = Score;
-	}
-
-    //calls the collision functions when the correct sprites collide
+	fill(255, 255, 255);
+	text('Score: '+Score, GAMEWIDTH/10, GAMEHEIGHT/6);
+	text('Highscore: '+Highscore, GAMEWIDTH/10, GAMEHEIGHT/3);	
+	//calls the corresponding function when an asteroid collides with something
 	asteroidGroup.collides(floor, AsteroidHitFloor);
 	asteroidGroup.collides(wall1, AsteroidHitFloor);
 	asteroidGroup.collides(wall2, AsteroidHitFloor);
-	asteroidGroup.collides(player, AsteroidHitPlayer)
-
-	//makes the player move using left and right arrow keys, or a and d keys
+	asteroidGroup.collides(player, Gameover)
+	//makes mouse visible if the game is stopped and invisible if the game isnt stopped
+	if (stopgame == true) {
+		mouse.visible = true;
+	} else {
+		mouse.visible = false;
+	}
+	//If statement for start button
+	if (mouse.pressing() && reset.mouse.hovering()) {
+		//starts the game again
+		stopgame = false;
+		//removes reset button, instructions button, and all balls added to represent your score
+		reset.remove();
+		Instructions.remove();
+		Scoregroup.removeAll();
+		//resets your time and score
+		time = 0;
+		Score = 0;
+	}
+	//adds a timer that goes up
+	time++;
+	//if statement for when asteroids should be made based off of the stopgame variable and the timer
+	if (time == 10 && stopgame == false) {
+		MakeAsteroid((random(30, 100)));
+		//resets time when asteroid spawns so timer can reach the correct number for a new asteroid to spawn 
+		time = 0;			
+	}
+	//sets the score as the highscore when the score is higher then the highscore
+	if (Score >= Highscore) {
+		Highscore = Score;
+	}
+	//all controls for the player, when the A or left keys are pressed the player moves left, when the D or right key is pressed the player moves right
 	if (kb.pressing('left') && stopgame == false) {
     	player.vel.x = -8;
 		player.rotationSpeed = 0;
